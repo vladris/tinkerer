0cdc889bf633ba475c80486a1bfa590d88ce471b
Vlad Riscutia <riscutiavlad@gmail.com>
2011-12-18 19:48:26 -0800

    Separate functions to handle updating the master doc

---
+++ tinkerer/master.py
@@ -0,0 +1,76 @@
+'''
+    master
+    ~~~~~~
+
+    Handles updating the master document.
+
+    :copyright: Copyright 2011 by Vlad Riscutia
+    :license: FreeBSD, see LICENSE file
+'''
+from tinkerer import paths
+
+
+
+def read_master():
+    '''
+    Reads master file into a list.
+    '''
+    with open(paths.master_file, "r") as f:
+        return f.readlines()
+
+
+
+def write_master(lines):
+    '''
+    Overwrites master file with given lines.
+    '''
+    with open(paths.master_file, "w") as f:
+        f.writelines(lines)
+
+
+
+def prepend_doc(docname):
+    '''
+    Inserts document at the top of the TOC.
+    '''
+    lines = read_master()
+
+    # find maxdepth directive
+    for line_no, line in enumerate(lines):
+        if "maxdepth" in line:
+            break
+
+    # insert docname after it with 3 space alignement
+    lines.insert(line_no + 2, "   %s\n" % docname)
+    
+    write_master(lines)
+
+
+
+def append_doc(docname):
+    '''
+    Appends document at the end of the TOC.
+    '''
+    lines = read_master()
+
+    # find second blank line after maxdepth directive
+    blank = 0    
+    for line_no, line in enumerate(read_master()):
+        if blank == 3: break
+        if "maxdepth" in line: blank = 1
+        if blank and line == "\n": blank += 1
+
+    lines.insert(line_no, "   %s\n" % docname)
+
+    write_master(lines) 
+   
+    
+    
+def remove_doc(docname):
+    '''
+    Removes document from the TOC.
+    '''
+    # rewrite file filtering line containing docname
+    write_master(filter(
+            lambda line: line != "   %s\n" % docname, 
+            read_master()))
---
+++ tinkertest/test_master.py
@@ -0,0 +1,100 @@
+'''
+    Master Document Update Test
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests updating the master document.
+
+    :copyright: Copyright 2011 by Vlad Riscutia
+    :license: FreeBSD, see LICENSE file
+'''
+from tinkerer import master
+import utils
+
+
+# test updating master document
+class TestMaster(utils.BaseTinkererTest):
+    # head and tail of master doc checked in each test
+    MASTER_HEAD = [
+            "Sitemap\n",
+            "=======\n",
+            "\n",
+            ".. toctree::\n",
+            "   :maxdepth: 1\n",
+            "\n"]
+
+
+    MASTER_TAIL = ["\n"]
+
+
+    # validate master doc created by setup
+    def test_setup(self):
+        self.assertEquals(
+                TestMaster.MASTER_HEAD + TestMaster.MASTER_TAIL,
+                master.read_master())
+
+
+    # test appending at the end of the TOC
+    def test_append(self):
+        new_docs = ["somewhere/somedoc.rst", "anotherdoc.rst"]
+
+        master.append_doc(new_docs[0])
+
+        # first doc should be appendend in the correct place
+        self.assertEquals(
+                    TestMaster.MASTER_HEAD + 
+                    ["   %s\n" % new_docs[0]] + 
+                    TestMaster.MASTER_TAIL,
+                master.read_master())
+
+        master.append_doc(new_docs[1])
+
+        # second doc should be appended in the correct place
+        self.assertEquals(
+                    TestMaster.MASTER_HEAD +
+                    ["   %s\n" % new_docs[0], "   %s\n" % new_docs[1]] +
+                    TestMaster.MASTER_TAIL,
+                master.read_master())
+
+
+
+    # test prepending at the beginning of the TOC
+    def test_prepend(self):
+        new_docs = ["somewhere/somedoc.rst", "anotherdoc.rst"]
+
+        # first doc should be prepended in the correct place
+        master.prepend_doc(new_docs[0])
+
+        self.assertEquals(
+                    TestMaster.MASTER_HEAD +
+                    ["   %s\n" % new_docs[0]] +
+                    TestMaster.MASTER_TAIL,
+                master.read_master())
+
+        master.prepend_doc(new_docs[1])
+
+        # order should be second doc then first doc
+        self.assertEquals(
+                    TestMaster.MASTER_HEAD +
+                    ["   %s\n" % new_docs[1], "   %s\n" % new_docs[0]] +
+                    TestMaster.MASTER_TAIL,
+                master.read_master())
+
+
+    # test removing from the TOC
+    def test_remove(self):
+        # append 4 docs
+        new_docs = ["a.rst", "b.rst", "c.rst", "d.rst"]
+        for doc in new_docs:
+            master.append_doc(doc)
+
+        # remove 3 of them while checking master each time
+        for doc_to_remove in ["c.rst", "b.rst", "d.rst"]:
+            master.remove_doc(doc_to_remove)
+            new_docs.remove(doc_to_remove)
+
+            self.assertEquals(
+                        TestMaster.MASTER_HEAD +
+                        ["   %s\n" % doc for doc in new_docs] +
+                        TestMaster.MASTER_TAIL,
+                    master.read_master())
+
