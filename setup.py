b60db7a1bd2bb03aca98d11512d948d3a9db38ca
Vlad Riscutia <riscutiavlad@gmail.com>
2012-02-01 22:18:06 -0800

    Update MANIFEST.in, cleanup setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     ],
     platforms = "any",
     packages = find_packages(exclude=["tinkertest"]),
-    package_data = {"" : ["locale/*/LC_MESSAGES/*.mo"]},
     include_package_data = True,
     entry_points = {
         "console_scripts": [
