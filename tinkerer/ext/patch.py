7690bb88e37a892e742ee778836fc3fd5877bb28
Vlad Riscutia <riscutiavlad@gmail.com>
2012-02-28 23:17:25 -0800

    HTML-to-XML entity patch

--- tinkerer/ext/patch.py
+++ tinkerer/ext/patch.py
@@ -10,7 +10,73 @@
     CONTRIBUTORS file)
     :license: FreeBSD, see LICENSE file
 '''
+import re
 import xml.dom.minidom
+
+try:
+    from html.entities import name2codepoint
+except:
+    from htmlentitydefs import name2codepoint    
+
+try:
+    import builtins as __builtin__
+except:
+    import __builtin__
+    
+
+# check whether unichr builtin exists, otherwise use chr
+if "unichr" not in __builtin__.__dict__:
+    unichr = chr
+    
+            
+
+def build_html_only_codepoints():
+    """
+    Convert well formed HTML to XML by removing HTML only entity definitions.
+    """
+    html_name2codepoints = {}
+    html_name2codepoints.update(name2codepoint)
+    for name in ['amp', 'gt', 'lt', 'apos', 'quot']:
+        if name in html_name2codepoints:
+            del html_name2codepoints[name]
+    return html_name2codepoints
+
+
+
+HTML_ONLY_NAME2CODEPOINTS = build_html_only_codepoints()
+
+
+
+def convert(s):
+    """
+    Take an input string s, find all things that look like SGML character 
+    entities, and replace them with the Unicode equivalent.
+
+    Source:
+    http://stackoverflow.com/questions/1197981/convert-html-entities-to-ascii-in-python/1582036#1582036
+    """
+    matches = re.findall("&#\d+;", s)
+    if len(matches) > 0:
+        hits = set(matches)
+        for hit in hits:
+            name = hit[2:-1]
+            try:
+                entnum = int(name)
+                s = s.replace(hit, unichr(entnum))
+            except ValueError:
+                pass
+    matches = re.findall("&\w+;", s)
+    hits = set(matches)
+    amp = "&"
+    if amp in hits:
+        hits.remove(amp)
+    for hit in hits:
+        name = hit[1:-1]
+        if name in HTML_ONLY_NAME2CODEPOINTS:
+            s = s.replace(hit,
+                          unichr(HTML_ONLY_NAME2CODEPOINTS[name]))
+    s = s.replace(amp, "&")
+    return s
 
 
 
@@ -20,8 +86,8 @@ def patch_links(body, docpath, docname=None, link_title=False):
     to fix hyperlinks. Also hyperlinks document title. Returns resulting 
     XML as string.
     '''
-    doc = xml.dom.minidom.parseString(body.encode("utf-8"))
-
+    in_str = convert(body).encode("utf-8")
+    doc = xml.dom.minidom.parseString(in_str)
     patch_node(doc, docpath)
 
     if link_title:
