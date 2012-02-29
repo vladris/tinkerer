15a77c6d0d9d093397a3cf398ed7920c4bb52903
Vlad Riscutia <riscutiavlad@gmail.com>
2012-02-28 18:43:43 -0800

    Update test_patch to expect different values on different Python versions to account for Sphinx relative link inconsistency

--- tinkertest/test_patch.py
+++ tinkertest/test_patch.py
@@ -12,6 +12,7 @@ import datetime
 import os
 from tinkerer import paths, post
 import utils
+import sys
 
 
 # test case
@@ -34,23 +35,31 @@ class TestPatch(utils.BaseTinkererTest):
         # build and check output
         self.build()
 
+        # for some reason Sphinx on Python 3 creates different relative links:
+        # Post1 referencing Post2 which is in same dir results in a href like
+        # "../01/post2.html" while on Python 2 href is "post2.html"
+        python3 = sys.version_info[0] == 3
+
+
         # validate output - each tuple represents a file and the list of 
         # strings expected in the file
         tests = [(["2010", "10", "01", "post1.html"], 
-                    # for some reason Sphinx links ../01/post2 instead of post2
-                    ['href="../01/post2.html#x"',
+                    ['href="../01/post2.html#x"' if python3 else 
+                                'href="post2.html"',
                      'href="www.archlinux.org"']),
                  (["2010", "10", "01", "post2.html"],
                     # images get places in _images directory under root
                     ['src="../../../_images/img.png"']),
                  (["index.html"],
                     # index.html should have links patched with relative address
-                    ['href="2010/10/01/../01/post2.html#x"',
+                    ['href="2010/10/01/../01/post2.html#x"' if python3 else 
+                                'href="2010/10/01/post2.html#x"',
                      'href="www.archlinux.org"',
                      'src="2010/10/01/../../../_images/img.png"']),
                  (["rss.html"],
                     # RSS feed should have links patched with absolute address
-                    ['href="http://127.0.0.1/blog/html/2010/10/01/../01/post2.html#x',
+                    ['href="http://127.0.0.1/blog/html/2010/10/01/../01/post2.html#x' if python3 else
+                                'href="http://127.0.0.1/blog/html/2010/10/01/post2.html#x"',
                      'href="www.archlinux.org"',
                      'src="http://127.0.0.1/blog/html/2010/10/01/../../../_images/img.png"'])]
 
