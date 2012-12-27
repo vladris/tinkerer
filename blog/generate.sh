5b65ace7587094b9d9b52f76c7c6dc33d8c643ae
Christian Jann <christian.jann@ymail.com>
2012-12-27 15:39:14 +0100

    Add a theme gallery

---
+++ blog/generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d ./themes ]; then mkdir ./themes; fi;
+
+DEFAULT_THEME="modern5"
+
+OTHER_THEMES="minimal
+boilerplate
+responsive
+dark
+modern
+"
+
+CURRENT_THEME=$DEFAULT_THEME
+
+for THEME in $OTHER_THEMES
+do
+  sed -i 's/'$CURRENT_THEME'/'$THEME'/g' conf.py
+  CURRENT_THEME=$THEME
+  echo "current theme: $CURRENT_THEME"
+  tinker -b
+  mv blog/html themes/$THEME
+done
+
+sed -i 's/'$CURRENT_THEME'/'$DEFAULT_THEME'/g' conf.py
+tinker -b
+mv themes blog/html/
