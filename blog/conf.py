8422db36146915fff70d9a98e2a180a25f9bde97
Vlad Riscutia <riscutiavlad@gmail.com>
2013-01-27 14:37:00 -0800

    Update Tinkerer blog config

--- blog/conf.py
+++ blog/conf.py
@@ -3,8 +3,9 @@ import tinkerer.paths
 
 project = 'Tinkerer'                   
 tagline = 'Blogging for Pythonistas'                  
+description = "Tinkerer is a Python blogging engine/static website generator powered by Sphinx"
 author = 'Vlad Riscutia'
-copyright = '2011, ' + author         
+copyright = '2011-2013, ' + author         
 website = 'http://tinkerer.me/'                              
 
 disqus_shortname = 'tinkerer'                                   
