fb30aceab9294a79c0ce6bf44889fd7a69d1c90e
Vlad Riscutia <riscutiavlad@gmail.com>
2013-01-27 20:37:58 -0800

    Use _themes instead of _theme for theme subdir to have consistent naming

--- tinkerer/__templates/conf.py
+++ tinkerer/__templates/conf.py
@@ -62,7 +62,7 @@ templates_path = ['_templates']
 html_static_path = ['_static', tinkerer.paths.static]
 
 # Add other theme paths here
-html_theme_path = ['_theme', tinkerer.paths.themes]                 
+html_theme_path = ['_themes', tinkerer.paths.themes]                 
 
 # Add file patterns to exclude from build
 exclude_patterns = ["drafts/*", "_templates/*"]
