3e96193e0e46e5f4ed7ebac230b43a8a380e1714
Vlad Riscutia <riscutiavlad@gmail.com>
2013-05-27 14:25:10 -0700

    Update CONTRIBUTORS. Better comment in conf.py.

--- tinkerer/__templates/conf.py
+++ tinkerer/__templates/conf.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import tinkerer
-import tinkerer.paths        
+import tinkerer.paths
 
 # **************************************************************
 # TODO: Edit the lines below
 # **************************************************************
 
 # Change this to the name of your blog
-project = 'My blog'                   
+project = 'My blog'
 
 # Change this to the tagline of your blog
-tagline = 'Add intelligent tagline here'                  
+tagline = 'Add intelligent tagline here'
 
 # Change this to the description of your blog
 description = 'This is an awesome blog'
@@ -20,32 +20,32 @@ description = 'This is an awesome blog'
 author = 'Winston Smith'
 
 # Change this to your copyright string
-copyright = '1984, ' + author         
+copyright = '1984, ' + author
 
 # Change this to your blog root URL (required for RSS feed)
-website = 'http://127.0.0.1/blog/html/'                              
+website = 'http://127.0.0.1/blog/html/'
 
 # **************************************************************
 # More tweaks you can do
 # **************************************************************
 
 # Add your Disqus shortname to enable comments powered by Disqus
-disqus_shortname = None                                   
+disqus_shortname = None
 
 # Change your favicon (new favicon goes in _static directory)
-html_favicon = 'tinkerer.ico'           
+html_favicon = 'tinkerer.ico'
 
 # Pick another Tinkerer theme or use your own
 html_theme = "{{ theme }}"
 
 # Theme-specific options, see docs
-html_theme_options = { }                                  
+html_theme_options = { }
 
 # Link to RSS service like FeedBurner if any, otherwise feed is
 # linked directly
 rss_service = None
 
-# Generate full posts (without "read more" link) for RSS feed
+# Generate full posts for RSS feed even when using "read more"
 rss_generate_full_posts = False
 
 # Number of blog posts per page
@@ -56,7 +56,7 @@ posts_per_page = 10
 # **************************************************************
 
 # Add other Sphinx extensions here
-extensions = [{{ extensions }}] 
+extensions = [{{ extensions }}]
 
 # Add other template paths here
 templates_path = ['_templates']
@@ -65,7 +65,7 @@ templates_path = ['_templates']
 html_static_path = ['_static', tinkerer.paths.static]
 
 # Add other theme paths here
-html_theme_path = ['_themes', tinkerer.paths.themes]                 
+html_theme_path = ['_themes', tinkerer.paths.themes]
 
 # Add file patterns to exclude from build
 exclude_patterns = ["drafts/*", "_templates/*"]
@@ -76,7 +76,7 @@ html_sidebars = {
 }
 
 # **************************************************************
-# Do not modify below lines as the values are required by 
+# Do not modify below lines as the values are required by
 # Tinkerer to play nice with Sphinx
 # **************************************************************
 
