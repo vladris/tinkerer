vladris/tinkerer

commit bd4ccbf6bb9da7992f2bda4591148a83d0adf64c

Add the "Move with images" feature.

tinkerer/draft.py
@@ -28,10 +28,6 @@ def create(title):
                         "drafts"), 
                     name + tinkerer.source_suffix)
 
-    if os.path.exists(path):
-        raise Exception("Draft '%s' already exists at '%s" %
-                        (title, path))
-
     writer.render(paths.post_template, path,
             { "title"     : title,
               "content"   : "",
@@ -56,7 +52,9 @@ def move(path):
     draft = os.path.join(utils.get_path(paths.root, "drafts"), filename)
 
     # move file
-    shutil.move(path, draft)
+    # shutil.move(path, draft)
+    target_dir = utils.get_path(paths.root, "drafts")
+    utils.move(path, target_dir)
 
     # check if file is a post or a page
     if os.path.basename(dirname) == "pages":

tinkerer/post.py
@@ -87,10 +87,13 @@ def move(path, date=None):
     Moves a post given its path.
     '''
     post = Post(title=None, path=path, date=date)
-    if os.path.exists(post.path):
-        raise Exception("Post '%s' already exists at '%s" %
-                        (title, post.path))
-    shutil.move(path, post.path)
+    # if os.path.exists(post.path):
+    #     raise Exception("Post '%s' already exists at '%s" %
+    #                     (title, post.path))
+    # shutil.move(path, post.path)
+    target_dir = os.path.dirname(post.path)
+    utils.move(path, target_dir)
+
     if not master.exists_doc(post.docname):
         master.prepend_doc(post.docname)
     return post

tinkerer/utils.py
@@ -11,8 +11,11 @@
 import datetime
 import os
 import re
+import shutil
 
-
+from docutils.core import publish_doctree
+from docutils.nodes import GenericNodeVisitor
+import docutils.parsers.rst.directives
 
 def name_from_title(title):
     '''
@@ -52,3 +55,77 @@ def split_date(date=None):
         date = datetime.datetime.today()
 
     return date.strftime("%Y/%m/%d").split("/")
+
+# ============== About Move a Post ===============
+
+def related_images(post_path):
+    '''
+    find all related images about the post, return images filename. 
+    '''
+    # reload for directive register re-initialize
+    try:
+        from imp import reload
+    except ImportError:
+        pass
+    reload(docutils.parsers.rst.directives)
+
+    class ImageExtractor(GenericNodeVisitor):
+        def __init__(self, doctree):
+            super().__init__(doctree)
+            self.uris = set()
+
+        def default_visit(self, node):
+            if node.tagname == 'image':
+                self.uris.add(node.get('uri'))
+            return True
+
+    # Read file
+    with open(post_path, encoding = 'utf8') as f:
+        string = f.read()
+
+    # Parse file
+    doctree = publish_doctree(string, settings_overrides = {'report_level': 5})
+    ie = ImageExtractor(doctree)
+    doctree.walk(ie)
+    images = ie.uris # All images be used in reST
+
+    # Remove "not in the same folder" images and empty filename
+    images = [img for img in images if
+         img and (os.path.dirname(img) == '' or os.path.dirname(img) == '.')]
+
+    return images
+
+def move(original_post, target_dir):
+    '''
+    Move a reST post to another folder, include images.
+    '''
+    original_dir, post_filename = os.path.split(original_post)
+
+    # Get related images filename
+    images = related_images(original_post)
+
+    # Calc source and target position.
+    here_and_there = [item for item in zip(
+        [os.path.join(original_dir, img) for img in images],
+        [os.path.join(target_dir, img) for img in images])]
+
+    # Avoid overwrite or file lost.
+    for here, there in here_and_there:
+        if not os.path.exists(here):
+            Exception('Attach image "%s" are not exist!' % here)
+        elif os.path.exists(there):
+            Exception('Image "%s" exists at "%s"!' %
+                      (os.path.basename(there), target_dir))
+
+    # Avoid ... for .reST file.
+    target_post = os.path.join(target_dir, post_filename)
+    if not os.path.exists(original_post):
+        Exception('Post file are not exists!')
+    elif os.path.exists(target_post):
+        Exception('Post "%s" already exists at "%s"' % 
+                  (post_filename, target_dir))
+
+    # Real move
+    here_and_there.append((original_post, target_post))
+    for here, there in here_and_there:
+        shutil.move(here, there)

commit 9f71838c38c61caeeb04be39eea390f64218ca35

Fixed: the drafts overwrite function broken in bd4ccbf6bb9da7992f2bda4591148a83d0adf64c

tinkerer/draft.py
@@ -28,6 +28,10 @@ def create(title):
                         "drafts"), 
                     name + tinkerer.source_suffix)
 
+    if os.path.exists(path):
+        raise Exception("Draft '%s' already exists at '%s" %
+                        (title, path)) 
+
     writer.render(paths.post_template, path,
             { "title"     : title,
               "content"   : "",
@@ -52,7 +56,6 @@ def move(path):
     draft = os.path.join(utils.get_path(paths.root, "drafts"), filename)
 
     # move file
-    # shutil.move(path, draft)
     target_dir = utils.get_path(paths.root, "drafts")
     utils.move(path, target_dir)
 

tinkerer/post.py
@@ -87,10 +87,7 @@ def move(path, date=None):
     Moves a post given its path.
     '''
     post = Post(title=None, path=path, date=date)
-    # if os.path.exists(post.path):
-    #     raise Exception("Post '%s' already exists at '%s" %
-    #                     (title, post.path))
-    # shutil.move(path, post.path)
+
     target_dir = os.path.dirname(post.path)
     utils.move(path, target_dir)
 

tinkerer/utils.py
@@ -17,6 +17,7 @@ from docutils.core import publish_doctree
 from docutils.nodes import GenericNodeVisitor
 import docutils.parsers.rst.directives
 
+
 def name_from_title(title):
     '''
     Returns a doc name from a title by replacing all characters which are not
@@ -56,7 +57,11 @@ def split_date(date=None):
 
     return date.strftime("%Y/%m/%d").split("/")
 
+
+
 # ============== About Move a Post ===============
+
+
 
 def related_images(post_path):
     '''